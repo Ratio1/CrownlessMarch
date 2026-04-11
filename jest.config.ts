db2aa09b9a244402956a246914e2022ae1180148
Andrei Damian <andrei@lummetry.ai>
2026-04-11 08:52:45 +0300

    fix: ignore build output in Thornwrithe Jest

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,7 @@ const createJestConfig = nextJest({ dir: './' });
 const customJestConfig = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/.next'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@/app/(.*)$': '<rootDir>/app/$1',
